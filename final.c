f04a5a37ac168408e00cc0d64f9490660e065473
Levon Stepanyan <levonmll@gmail.com>
2020-11-08 21:56:19 +0100

    norm is not complete

--- final.c
+++ final.c
@@ -12,24 +12,23 @@
 
 #include "fdf.h"
 
-#include <stdio.h>
 void	drow_horizontal(t_iso *abs, t_data data)
- {
-   while (abs->next)
-        {
-	  if (abs->content_size > 0) 
-		  drow_line(*abs, *abs->next, data);
-	  abs = abs->next;
-		
+{
+	while (abs->next)
+	{
+		if (abs->content_size > 0)
+			drow_line(*abs, *abs->next, data);
+		abs = abs->next;
 	}
- }
-void  drow_vertical(t_iso *abs, t_iso *ord, t_data data)
+}
+
+void	drow_vertical(t_iso *abs, t_iso *ord, t_data data)
 {
 	t_iso *abs_temp;
 	t_iso *ord_temp;
 
 	while (ord)
-	{  
+	{
 		abs_temp = abs;
 		ord_temp = ord;
 		while (abs_temp->content_size > 0 && ord_temp->content_size > 0)
@@ -41,13 +40,11 @@ void  drow_vertical(t_iso *abs, t_iso *ord, t_data data)
 		drow_line(*abs_temp, *ord_temp, data);
 		abs = next_row(abs);
 		ord = next_row(ord);
-	    }
 	}
+}
 
 void	final(t_iso *abs, t_iso *ord, t_data data)
 {
-
 	drow_horizontal(abs, data);
 	drow_vertical(abs, ord, data);
-
 }
--- get_map.c
+++ get_map.c
@@ -16,7 +16,7 @@
 
 int	validate_arr(char *arr)
 {
-	int	i;
+	int			i;
 
 	i = 0;
 	if ((arr[i] == '-' && (arr[i + 1] > '0' && arr[i + 1] <= '9'))	\
@@ -32,8 +32,8 @@ int	validate_arr(char *arr)
 
 int	*convert(char *str, float *count)
 {
-	int	*arr;
-	char	**split;
+	char			**split;
+	int			*arr;
 
 	if (!(split = ft_strsplit(str, ' ')))
 		return (NULL);
@@ -61,12 +61,12 @@ int	*convert(char *str, float *count)
 
 t_list	*get_map(int fd, t_data data, t_list *map, t_list *temp)
 {
-	t_list	*curr_node;
+	t_list			*curr_node;
 
 	data.count = 0;
 	while (get_next_line(fd, &data.line) == 1)
 	{
-		if (!(data.line_int = convert(data.line, &data.x))	\
+		if (!(data.line_int = convert(data.line, &data.x))		\
 		|| !(curr_node = ft_lstnew(data.line_int, data.x)))
 			return (NULL);
 		if (data.count == 0)
